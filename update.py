ae351e54fe55891f7b0214dfd15e3789db3f1501
Wok <woctezuma@users.noreply.github.com>
2022-12-22 22:02:17 +0100

    Fix bug: sorted gifts were returned but not assigned

--- update.py
+++ update.py
@@ -12,7 +12,7 @@ def main():
 
     post_new_gifts_to_discord(gifts, previous_gifts)
 
-    smart_merge_of_gifts(gifts, previous_gifts)
+    gifts = smart_merge_of_gifts(gifts, previous_gifts)
     save_gifts(gifts)
 
 
