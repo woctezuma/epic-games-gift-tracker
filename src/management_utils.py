1cb076731013cd526861a8c1533ae406dd130a30
Wok <woctezuma@users.noreply.github.com>
2022-12-22 21:43:17 +0100

    Fix bug: copy the timestamp from previous gifts

--- src/management_utils.py
+++ src/management_utils.py
@@ -1,10 +1,12 @@
 from src.comparison_utils import sort_gifts, is_among_gifts
-from src.time_utils import is_a_reliable_gift
+from src.time_utils import is_a_reliable_gift, copy_timestamps
 
 
 def smart_merge_of_gifts(gifts, previous_gifts):
     for gift in previous_gifts:
-        if not is_among_gifts(gift, gifts) and not is_a_reliable_gift(gift):
+        if is_among_gifts(gift, gifts):
+            copy_timestamps(gift, gifts)
+        elif not is_a_reliable_gift(gift):
             # We are unsure whether the deletion of the previous gift is due to
             # a bug on Epic Games' side, so we prefer to keep the gift for now.
             gifts.append(gift)
