ca75b5a8028c85e1150cb0f7d8e6fb72bba1b1fd
Wok <woctezuma@users.noreply.github.com>
2022-12-22 19:57:26 +0100

    Allow to merge gifts based on the time when they were first seen

--- src/management_utils.py
+++ src/management_utils.py
@@ -1,3 +1,5 @@
+from src.time_utils import is_a_reliable_gift
+
 SORTING_FIELD = "urlSlug"
 IDENTITY_FIELD = "title"
 
@@ -8,3 +10,13 @@ def sort_gifts(gifts):
 
 def is_among_gifts(gift, gifts):
     return any(gift[IDENTITY_FIELD] == e[IDENTITY_FIELD] for e in gifts)
+
+
+def smart_merge_of_gifts(gifts, previous_gifts):
+    for gift in previous_gifts:
+        if not is_among_gifts(gift, gifts) and not is_a_reliable_gift(gift):
+            # We are unsure whether the deletion of the previous gift is due to
+            # a bug on Epic Games' side, so we prefer to keep the gift for now.
+            gifts.append(gift)
+
+    return sort_gifts(gifts)
