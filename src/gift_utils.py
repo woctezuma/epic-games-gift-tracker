727069e44be8839e14ba9d46491d8fe04c385643
Wok <woctezuma@users.noreply.github.com>
2022-12-23 19:09:59 +0100

    Compare image urls directly, instead of relying on gift titles

--- src/gift_utils.py
+++ src/gift_utils.py
@@ -1,7 +1,7 @@
 from src.comparison_utils import is_among_gifts
 from src.discord_utils import post_message_to_discord_using_keyword
 from src.message_utils import to_message
-from src.parse_gift import parse_gift_wrapping_image_url_if_still_wrapped
+from src.parse_gift import get_image_urls_of_intact_wrappings
 from src.utils import unique
 from src.webhook_utils import WEBHOOK_KEYWORD_GIFT, WEBHOOK_KEYWORD_WRAPPING
 
@@ -18,9 +18,10 @@ def post_new_gifts_to_discord(gifts, previous_gifts):
 
 
 def post_new_gift_wrapping_images_to_discord(gifts, previous_gifts):
-    image_urls = [
-        parse_gift_wrapping_image_url_if_still_wrapped(gift) for gift in gifts if is_a_new_gift(gift, previous_gifts)
-    ]
+    current_image_urls = get_image_urls_of_intact_wrappings(gifts)
+    previous_image_urls = get_image_urls_of_intact_wrappings(previous_gifts)
+
+    image_urls = set(current_image_urls).difference(previous_image_urls)
 
     for image_url in unique(image_urls):
         if image_url is not None:
