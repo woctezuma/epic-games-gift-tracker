9cbc16e0c6b3e57c5b556f5fe62819a0683750cd
Wok <woctezuma@users.noreply.github.com>
2022-12-23 09:23:55 +0100

    Add functions to parse image urls for gift wrappings

--- src/parse_gift.py
+++ src/parse_gift.py
@@ -2,6 +2,8 @@ MAX_LENGTH = 200
 MAIN_FIELDS = ["title", "urlSlug"]
 IMAGE_FIELD = "keyImages"
 ATTRIBUTE_FIELD = "customAttributes"
+PRODUCT_SLUG_FIELD = "productSlug"
+GIFT_WRAPPING_IMAGE_FIELD = "VaultClosed"
 
 
 def is_dummy_entry(str_value):
@@ -20,3 +22,19 @@ def parse_key_images(gift):
 def parse_custom_attributes(gift):
     attribute_data = gift[ATTRIBUTE_FIELD]
     return {e["key"]: e["value"] for e in attribute_data if not is_dummy_entry(e["value"])}
+
+
+def parse_gift_wrapping_image_url(gift):
+    key_images = parse_key_images(gift)
+    return key_images.get(GIFT_WRAPPING_IMAGE_FIELD)
+
+
+def parse_gift_wrapping_image_url_if_still_wrapped(gift):
+    product_slug = gift.get(PRODUCT_SLUG_FIELD)
+
+    if is_dummy_entry(product_slug):
+        image_url = None
+    else:
+        image_url = parse_gift_wrapping_image_url(gift)
+
+    return image_url
