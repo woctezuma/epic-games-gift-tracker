784c0acc8bafd6f4dad95fd9c8e1682da8e1525d
Wok <woctezuma@users.noreply.github.com>
2022-12-23 09:40:16 +0100

    Post gift wrapping image URL to Discord

--- src/gift_utils.py
+++ src/gift_utils.py
@@ -1,7 +1,8 @@
 from src.comparison_utils import is_among_gifts
 from src.discord_utils import post_message_to_discord_using_keyword
 from src.message_utils import to_message
-from src.webhook_utils import WEBHOOK_KEYWORD_GIFT
+from src.parse_gift import parse_gift_wrapping_image_url_if_still_wrapped
+from src.webhook_utils import WEBHOOK_KEYWORD_GIFT, WEBHOOK_KEYWORD_WRAPPING
 
 
 def is_a_new_gift(gift, previous_gifts):
@@ -13,3 +14,7 @@ def post_new_gifts_to_discord(gifts, previous_gifts):
         if is_a_new_gift(gift, previous_gifts):
             message = to_message(gift, webhook_keyword=WEBHOOK_KEYWORD_GIFT)
             post_message_to_discord_using_keyword(message, webhook_keyword=WEBHOOK_KEYWORD_GIFT)
+
+            image_url = parse_gift_wrapping_image_url_if_still_wrapped(gift)
+            if image_url is not None:
+                post_message_to_discord_using_keyword(image_url, webhook_keyword=WEBHOOK_KEYWORD_WRAPPING)
