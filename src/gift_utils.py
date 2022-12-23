8663ebf4c463a693143a0cf6e87b5e09ce54178c
Wok <woctezuma@users.noreply.github.com>
2022-12-23 10:26:54 +0100

    Avoid posting duplicate gift wrapping image URL

--- src/gift_utils.py
+++ src/gift_utils.py
@@ -2,6 +2,7 @@ from src.comparison_utils import is_among_gifts
 from src.discord_utils import post_message_to_discord_using_keyword
 from src.message_utils import to_message
 from src.parse_gift import parse_gift_wrapping_image_url_if_still_wrapped
+from src.utils import unique
 from src.webhook_utils import WEBHOOK_KEYWORD_GIFT, WEBHOOK_KEYWORD_WRAPPING
 
 
@@ -10,6 +11,7 @@ def is_a_new_gift(gift, previous_gifts):
 
 
 def post_new_gifts_to_discord(gifts, previous_gifts):
+    image_urls = []
     for gift in gifts:
         if is_a_new_gift(gift, previous_gifts):
             message = to_message(gift, webhook_keyword=WEBHOOK_KEYWORD_GIFT)
@@ -17,4 +19,7 @@ def post_new_gifts_to_discord(gifts, previous_gifts):
 
             image_url = parse_gift_wrapping_image_url_if_still_wrapped(gift)
             if image_url is not None:
-                post_message_to_discord_using_keyword(image_url, webhook_keyword=WEBHOOK_KEYWORD_WRAPPING)
+                image_urls.append(image_url)
+
+    for image_url in unique(image_urls):
+        post_message_to_discord_using_keyword(image_url, webhook_keyword=WEBHOOK_KEYWORD_WRAPPING)
