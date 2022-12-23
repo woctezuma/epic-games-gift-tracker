6c00a5c871088413aa590ea5f2f592881f43b4e5
Wok <woctezuma@users.noreply.github.com>
2022-12-23 09:05:22 +0100

    Refactoring: add a function to check for dummy entries ("[]")

--- src/parse_gift.py
+++ src/parse_gift.py
@@ -4,11 +4,15 @@ IMAGE_FIELD = "keyImages"
 ATTRIBUTE_FIELD = "customAttributes"
 
 
+def is_dummy_entry(str_value):
+    return str_value == "[]"
+
+
 def parse_meta_data(gift, max_length=MAX_LENGTH):
     return {
         field: gift[field][:max_length]
         for field in MAIN_FIELDS
-        if field in gift and gift[field] is not None and gift[field] != "[]"
+        if field in gift and gift[field] is not None and not is_dummy_entry(gift[field])
     }
 
 
@@ -19,4 +23,4 @@ def parse_key_images(gift):
 
 def parse_custom_attributes(gift):
     attribute_data = gift[ATTRIBUTE_FIELD]
-    return {e["key"]: e["value"] for e in attribute_data if e["value"] != "[]"}
+    return {e["key"]: e["value"] for e in attribute_data if not is_dummy_entry(e["value"])}
