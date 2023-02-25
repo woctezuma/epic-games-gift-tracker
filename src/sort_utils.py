600b37af48c8197a7b9806fa6677ea8a229c952f
Wok <woctezuma@users.noreply.github.com>
2023-02-25 01:03:53 +0100

    Sort ratios in ascending order, so that larger discounts are first
    Caveat: discountPercentage is confusing, this is the ratio between discounted and base prices.

--- src/sort_utils.py
+++ src/sort_utils.py
@@ -4,7 +4,7 @@ PROMOTION_SUB_FIELDS = ["promotionalOffers", "upcomingPromotionalOffers"]
 
 def sort_promotions(promos):
     # Caveat: discountPercentage is confusing, this is the ratio between discounted and base prices.
-    return sorted(promos, key=lambda x: (x["startDate"], -x["discountSetting"]["discountPercentage"]))
+    return sorted(promos, key=lambda x: (x["startDate"], x["discountSetting"]["discountPercentage"]))
 
 
 def sort_promotions_inside_gifts(gifts):
